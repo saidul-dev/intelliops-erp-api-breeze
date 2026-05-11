15a73b771795cd69596056a876f97d21860da8ab
Saidul Islam <mdsaidulislam.dev@gmail.com>
2026-05-12 00:31:40 +0600

    Modified some logic

--- app/Http/Controllers/Api/SiteSettingController.php
+++ app/Http/Controllers/Api/SiteSettingController.php
@@ -10,7 +10,12 @@ class SiteSettingController extends Controller
 {
     public function index()
     {
-        $settings = SiteSetting::get();
+        $settings = SiteSetting::all()->mapWithKeys(function ($setting) {
+            if (in_array($setting->key, ['site_logo', 'site_favicon']) && $setting->value) {
+                return [$setting->key => asset('storage/' . $setting->value)];
+            }
+            return [$setting->key => $setting->value];
+        });
         return response()->json($settings);
     }
 
@@ -18,8 +23,8 @@ class SiteSettingController extends Controller
     {
         $validatedData = $request->validate([
             'site_name' => 'required|string|max:255',
-            'site_logo' => 'required|image|max:2048',
-            'site_favicon' => 'required|image|max:2048',
+            'site_logo' => 'nullable|image|max:2048',
+            'site_favicon' => 'nullable|image|max:2048',
             'site_description' => 'required|string',
             'site_keywords' => 'nullable|string',
             'site_author' => 'nullable|string|max:255',
@@ -27,6 +32,15 @@ class SiteSettingController extends Controller
             'site_phone' => 'nullable|string|max:20',
             'site_address' => 'nullable|string|max:500',
         ]);
+
+        // Handle file uploads for logo and favicon
+        if ($request->hasFile('site_logo')) {
+            $validatedData['site_logo'] = $request->file('site_logo')->store('site_settings', 'public');
+        }
+
+        if ($request->hasFile('site_favicon')) {
+            $validatedData['site_favicon'] = $request->file('site_favicon')->store('site_settings', 'public');
+        }
 
         foreach ($validatedData as $key => $value) {
             SiteSetting::updateOrCreate(
